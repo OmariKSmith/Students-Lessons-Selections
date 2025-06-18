f504282a50c581b8806807d079d4dfa6f5c4b6ad
omariksmith <o_smith5544@sbcglobal.net>
2025-06-18 18:16:47 -0500

    6182025 update 2

--- src/main/java/org/javaclass/exercises/OrderItem.java
+++ src/main/java/org/javaclass/exercises/OrderItem.java
@@ -1,5 +1,5 @@
 package org.javaclass.exercises;
-
+//!                             Build this class's toString, accessor, mutators methods.
 //!                             Alter the isInStock method to check quantity before
 //!                             returning it's value.
 //!                             If quantity is < 1 return false
